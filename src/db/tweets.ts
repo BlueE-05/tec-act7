93e6e9d15ac5afe93c0348a23de0382ef8cd2e97
BlueE-05 <danaetorres35@gmail.com>
2025-04-01 22:42:39 -0600

    Configuracion de la capa base de datos (simulada, en realidad son arrays)

---
+++ src/db/tweets.ts
@@ -0,0 +1,30 @@
+export default class TweetDB {
+    private tweets: any[] = [
+        { id: '1', text: 'Hello World' },
+        { id: '2', text: 'This is a tweet' },
+        { id: '3', text: 'listening to Dualipa while doin this' },
+        { id: '4', text: 'you want me, i want you baby. Im levitatiiingg' },
+        { id: '5', text: 'Training season' }
+    ];
+
+    async getAllTweets() {
+        return this.tweets;
+    }
+
+    async getTweetById(id: string) {
+        return this.tweets.find(tweet => tweet.id === id);
+    }
+
+    async updateTweet(id: string, data: string) {
+        const tweet = this.tweets.find(tweet => tweet.id === id);
+        if (tweet) {
+            Object.assign(tweet, data);
+            return tweet;
+        }
+        return null;
+    }
+    
+    async deleteTweet(id: string) {
+        this.tweets = this.tweets.filter(tweet => tweet.id !== id);
+    }
+}
